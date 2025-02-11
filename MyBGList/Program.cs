b5d6e577bff874d43a2f3fc166bf7f3c2fdac723
Joshua <gallopingbull89@gmail.com>
2025-02-10 18:17:56 -0800

    enabling CORS

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc.Formatters;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(cfg =>
+    {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+    options.AddPolicy(name: "AnyOrigin",
+        cfg => {
+            cfg.AllowAnyOrigin();
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -23,9 +40,11 @@ else
     app.UseExceptionHandler("/error");
 
 app.UseHttpsRedirection();
+app.UseCors("AnyOrigin");
 app.UseAuthorization();
-app.MapGet("/error", () => Results.Problem());
-//app.MapGet("/error/test", () => { throw new Exception("test"); });
-app.MapControllers();
+app.MapGet("/error", [EnableCors("AnyOrigin")] () => Results.Problem()).RequireCors("AnyOrigins");
+//app.MapGet("/error/test", [EnableCors("AnyOrigin")] () => { throw new Exception("test"); })W;
+app.MapControllers()
+    .RequireCors("AnyOrigin");
 
 app.Run();
