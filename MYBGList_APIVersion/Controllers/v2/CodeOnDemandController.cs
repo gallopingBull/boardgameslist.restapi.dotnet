e1750173c178785e2da2b62e094d487e8527082c
Joshua <gallopingbull89@gmail.com>
2025-03-06 21:27:14 -0800

    chapter 3 - versioning exercise

---
+++ MYBGList_APIVersion/Controllers/v2/CodeOnDemandController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MYBGList_APIVersion.Controllers.v2
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CodeOnDemandController : ControllerBase
+    {
+        [EnableCors("AnyOrigin_GetOnly")]
+        public void Get()
+        {
+            Results.Text("<script>" +
+                "window.alert('Your client supports Javascript!" +
+                "\\r\\n\\r\\n" +
+                $"Server time (UTC): {DateTime.UtcNow.ToString("o")}" +
+                "\\r\\n" +
+                "Client time (UTC): ' + new Date().toISOString());" +
+                "</script>" +
+                "<noscript>Your client does not support Javascript</noscript>", "text/html");
+        }
+    }
+}
---
+++ MYBGList_APIVersion/Program.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(cfg =>
+    {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+    options.AddPolicy(name: "AnyOrigin",
+        cfg => {
+            cfg.AllowAnyOrigin();
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+    });
+    options.AddPolicy(name: "AnyOrigin_GetOnly",
+        cfg => {
+            cfg.AllowAnyOrigin();
+            cfg.AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Configuration.GetValue<bool>("UseSwagger"))
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+    app.UseDeveloperExceptionPage();
+else
+    app.UseExceptionHandler("/error");
+
+app.UseHttpsRedirection();
+app.UseCors("AnyOrigin");
+app.UseAuthorization();
+app.MapGet("/error", 
+    [EnableCors("AnyOrigin")]
+    [ResponseCache(NoStore = true)] () => 
+    Results.Problem()).RequireCors("AnyOrigin");
+//app.MapGet("/error/test",
+//[EnableCors("AnyOrigin")]
+//[ResponseCache(NoStore = true)] () => { throw new Exception("test"); });
+app.MapGet("/cod/test",
+    [EnableCors("AnyOrigin_GetOnly")]
+    [ResponseCache(NoStore = true)] () =>
+    Results.Text("<script>" +
+    "window.alert('Your client supports Javascript!" +
+    "\\r\\n\\r\\n" +
+    $"Server time (UTC): {DateTime.UtcNow.ToString("o")}" +
+    "\\r\\n" +
+    "Client time (UTC): ' + new Date().toISOString());" +
+    "</script>" +
+    "<noscript>Your client does not support Javascript</noscript>", "text/html"));
+app.MapControllers()
+    .RequireCors("AnyOrigin");
+
+app.Run();
