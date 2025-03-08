81ed7be7702eef526b5a58e58d00660014391ec9
Joshua <gallopingbull89@gmail.com>
2025-03-08 15:20:47 -0800

    configuring entitities

--- MyBGList/Models/ApplicationDbContext.cs
+++ MyBGList/Models/ApplicationDbContext.cs
@@ -8,7 +8,6 @@ namespace MyBGList.Models
             DbContextOptions<ApplicationDbContext> options) 
             : base(options) 
         { 
-        
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -18,8 +17,35 @@ namespace MyBGList.Models
             modelBuilder.Entity<BoardGames_Domains>()
                 .HasKey(i => new { i.BoardGameId, i.DomainId });
 
+            modelBuilder.Entity<BoardGames_Domains>()
+                .HasOne(x => x.BoardGame)
+                .WithMany(y => y.BoardGames_Domains)
+                .HasForeignKey(f => f.BoardGameId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<BoardGames_Domains>()
+                .HasOne(o => o.Domain)
+                .WithMany(m => m.BoardGames_Domains)
+                .HasForeignKey(f => f.DomainId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
             modelBuilder.Entity<BoardGames_Mechanics>()
                 .HasKey(i => new { i.BoardGameId, i.MechanicId });
+
+            modelBuilder.Entity<BoardGames_Mechanics>()
+                .HasOne(x => x.BoardGame)
+                .WithMany(y => y.BoardGames_Mechanics)
+                .HasForeignKey(f => f.BoardGameId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<BoardGames_Mechanics>()
+                .HasOne(o => o.Mechanic)
+                .WithMany(m => m.BoardGames_Mechanics)
+                .HasForeignKey(f => f.MechanicId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
