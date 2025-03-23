cd56119720744820d7925e706d143784012bb518
Joshua <gallopingbull89@gmail.com>
2025-03-23 14:10:03 -0700

    added boardgamedto class

---
+++ MyBGList/DTO/v1/BoardGameDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MyBGList.DTO.v1
+{
+    namespace MyBGList.DTO
+    {
+        public class BoardGameDTO
+        {
+            [Required]
+            public int Id { get; set; }
+
+            public string? Name { get; set; }
+
+            public int? Year { get; set; }
+        }
+    }
+}
