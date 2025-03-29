9926372359aa4830fc22afd801b794a3752e70bf
Joshua <gallopingbull89@gmail.com>
2025-03-29 15:58:31 -0700

    Update Program.cs
    adding custom error messages for model binding

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -11,7 +11,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ModelBindingMessageProvider.SetValueIsInvalidAccessor(
+        (x) => $"The value '{x}' is invalid.");
+    options.ModelBindingMessageProvider.SetValueMustBeANumberAccessor(
+        (x) => $"The field {x} must be a number.");
+    options.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor(
+        (x, y) => $"The value '{x}' is not valid for {y}.");
+    options.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(
+        () => $"A value is required.");
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -69,6 +79,9 @@ builder.Services.AddCors(options =>
         });
 });
 
+// Code replaced by the [ManualValidationFilter] attribute
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+   options.SuppressModelStateInvalidFilter = true);
 
 var app = builder.Build();
 
