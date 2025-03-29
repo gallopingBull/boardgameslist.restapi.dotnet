a9e077859dcf4eeb36b57c32d47a63c1e12115d5
Joshua <gallopingbull89@gmail.com>
2025-03-29 15:04:37 -0700

    adding domain controller and dto

---
+++ MyBGList/Controllers/v1/MechanicsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBGList.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class MechanicsController : ControllerBase
+    {
+
+    }
+}
