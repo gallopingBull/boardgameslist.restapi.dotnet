116172bb33dbeb07315226d95e69c539e10d0f27
Joshua <gallopingbull89@gmail.com>
2025-04-11 11:14:38 -0700

    configuring and adding auth. services

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using MyBGList.Constants;
 using MyBGList.Models;
@@ -96,11 +99,50 @@ builder.Services.AddSwaggerGen(options =>
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddUserSecrets<Program>();  // This is crucial for reading secrets
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+////
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString)
     );
+
+builder.Services.AddIdentity<ApiUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 12;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+    options.DefaultChallengeScheme =
+    options.DefaultForbidScheme =
+    options.DefaultScheme =
+    options.DefaultSignInScheme =
+    options.DefaultSignOutScheme =
+        JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        RequireExpirationTime = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+          System.Text.Encoding.UTF8.GetBytes(
+              builder.Configuration["JWT:SigningKey"])
+      )
+    };
+});
 
 builder.Services.AddApiVersioning(options =>
 {
@@ -189,6 +231,7 @@ else
 app.UseHttpsRedirection();
 app.UseCors("AnyOrigin");
 app.UseResponseCaching();
+app.UseAuthentication();    
 app.UseAuthorization();
 
 // Adds a default cache-control directive
