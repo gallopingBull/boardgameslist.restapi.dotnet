e7d71c19d4bee17cc80b29374c1267d979593d5c
Joshua <gallopingbull89@gmail.com>
2025-04-11 11:24:45 -0700

    Update AccountController.cs
    removed comments

--- MyBGList/Controllers/v1/AccountController.cs
+++ MyBGList/Controllers/v1/AccountController.cs
@@ -42,54 +42,54 @@ namespace MyBGList.Controllers
             _signInManager = signInManager;
         }
 
-        //[HttpPost]
-        //[ResponseCache(CacheProfileName = "NoCache")]
-        //public async Task<ActionResult> Register(RegisterDTO input)
-        //{
-        //    try
-        //    {
-        //        if (ModelState.IsValid)
-        //        {
-        //            var newUser = new ApiUser();
-        //            newUser.UserName = input.UserName;
-        //            newUser.Email = input.Email;
-        //            var result = await _userManager.CreateAsync(
-        //                newUser, input.Password);
-        //            if (result.Succeeded)
-        //            {
-        //                _logger.LogInformation(
-        //                    "User {userName} ({email}) has been created.",
-        //                newUser.UserName, newUser.Email);
-        //                return StatusCode(201,
-        //                    $"User '{newUser.UserName}' has been created.");
-        //            }
-        //            else
-        //                throw new Exception(
-        //                    string.Format("Error: {0}", string.Join(" ",
-        //                        result.Errors.Select(e => e.Description))));
-        //        }
-        //        else
-        //        {
-        //            var details = new ValidationProblemDetails(ModelState);
-        //            details.Type =
-        //                    "https://tools.ietf.org/html/rfc7231#section-6.5.1";
-        //            details.Status = StatusCodes.Status400BadRequest;
-        //            return new BadRequestObjectResult(details);
-        //        }
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        var exceptionDetails = new ProblemDetails();
-        //        exceptionDetails.Detail = e.Message;
-        //        exceptionDetails.Status =
-        //            StatusCodes.Status500InternalServerError;
-        //        exceptionDetails.Type =
-        //                "https://tools.ietf.org/html/rfc7231#section-6.6.1";
-        //        return StatusCode(
-        //            StatusCodes.Status500InternalServerError,
-        //            exceptionDetails);
-        //    }
-        //}
+        [HttpPost]
+        [ResponseCache(CacheProfileName = "NoCache")]
+        public async Task<ActionResult> Register(RegisterDTO input)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var newUser = new ApiUser();
+                    newUser.UserName = input.UserName;
+                    newUser.Email = input.Email;
+                    var result = await _userManager.CreateAsync(
+                        newUser, input.Password);
+                    if (result.Succeeded)
+                    {
+                        _logger.LogInformation(
+                            "User {userName} ({email}) has been created.",
+                        newUser.UserName, newUser.Email);
+                        return StatusCode(201,
+                            $"User '{newUser.UserName}' has been created.");
+                    }
+                    else
+                        throw new Exception(
+                            string.Format("Error: {0}", string.Join(" ",
+                                result.Errors.Select(e => e.Description))));
+                }
+                else
+                {
+                    var details = new ValidationProblemDetails(ModelState);
+                    details.Type =
+                            "https://tools.ietf.org/html/rfc7231#section-6.5.1";
+                    details.Status = StatusCodes.Status400BadRequest;
+                    return new BadRequestObjectResult(details);
+                }
+            }
+            catch (Exception e)
+            {
+                var exceptionDetails = new ProblemDetails();
+                exceptionDetails.Detail = e.Message;
+                exceptionDetails.Status =
+                    StatusCodes.Status500InternalServerError;
+                exceptionDetails.Type =
+                        "https://tools.ietf.org/html/rfc7231#section-6.6.1";
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError,
+                    exceptionDetails);
+            }
+        }
         //
         //[HttpPost]
         //[ResponseCache(CacheProfileName = "NoCache")]
