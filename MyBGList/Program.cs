ec81a80740a5510fb970e5387b8ce9cd9a6cfd72
Joshua <gallopingbull89@gmail.com>
2025-04-13 16:26:31 -0700

    Update Program.cs
    adding  HTTP Security Headers

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -260,6 +260,27 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+    // HTTP Security Headers
+    app.UseHsts();
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Add("X-Frame-Options",
+            "sameorigin");
+        context.Response.Headers.Add("X-XSS-Protection",
+            "1; mode=block");
+        context.Response.Headers.Add("X-Content-Type-Options",
+            "nosniff");
+        context.Response.Headers.Add("Content-Security-Policy",
+            "default-src 'self'; script-src 'self' 'nonce-23a98b38c'");
+        context.Response.Headers.Add("Referrer-Policy",
+            "strict-origin");
+        await next();
+    });
+}
 
 if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
     app.UseDeveloperExceptionPage();
